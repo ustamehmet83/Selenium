7be21a9ca418026bc91ef8bc0bd584902f09aa6e
Mehmet Usta <ustamehmet83@hotmail.com>
2023-01-04 00:18:44 +0300

    day5 TestNG_Intro

--- src/test/java/com/cydeo/tests/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/TestNG_Intro.java
@@ -1,19 +1,46 @@
 package com.cydeo.tests;
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TestNG_Intro {
-
-
-
-    @Test
-    public void test(){
-
-        System.out.println("Test 1 is running....");
-
-
-
+    @BeforeClass
+    public void setupMethod(){
+        System.out.println("----->BeforeClass is running");
     }
+    @AfterClass
+    public void tearDownClass(){
+        System.out.println("----->AfterClass is running");
+    }
+
+
+
+
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println("--->BeforeMethod is running!");
+    }
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("--->AfterMethod is running!");
+    }
+
+    @Test(priority = 2)
+    public void test1(){
+        System.out.println("Test 1 is running....");
+    }
+
+
+    @Test(priority = 1)
+    public void test2(){
+        System.out.println("Test 2 is running....");
+    }
+
+
+
+
+
+
+
 
 
 }
