4e2d06864e38d5da355241356cb990aa813b3c1d
Mehmet Usta <ustamehmet83@hotmail.com>
2023-01-19 08:29:40 +0300

    day9 Singleton

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T2_Upload_Practice.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T2_Upload_Practice.java
@@ -9,20 +9,21 @@ import org.testng.annotations.Test;
 public class T2_Upload_Practice {
 
     @Test
-    public void upload_Test(){
+    public void upload_Test() {
 //        TC #2 Upload Test
 //        1. Go to https://practice.cydeo.com/upload
         Driver.getDriver().get("https://practice.cydeo.com/upload");
 //        2. Find some small file from your computer, and get the path of it.
-        String path="C:\\Users\\mehme\\OneDrive\\Desktop\\Yeni klasör";
+        String path = "C:\\Users\\mehme\\OneDrive\\Desktop\\Yeni klasör\\Baza Ayak Montaj Şablonu No 1.pdf";
 //        3. Upload the file.
-        WebElement fileUpload=Driver.getDriver().findElement(By.id("file-upload"));
+        WebElement fileUpload = Driver.getDriver().findElement(By.id("file-upload"));
         BrowserUtils.sleep(2);
         fileUpload.sendKeys(path);
-        WebElement uploadBtn=Driver.getDriver().findElement(By.xpath("//input[@id='file-submit']"));
+        WebElement uploadBtn = Driver.getDriver().findElement(By.xpath("//input[@id='file-submit']"));
         uploadBtn.click();
-//        4. Assert:
-//        -File uploaded text is displayed on the page
+//        4. Assert:-File uploaded text is displayed on the page
+        WebElement fileUploadedHeader = Driver.getDriver().findElement(By.id("uploaded-files"));
+        fileUploadedHeader.isDisplayed();
     }
 
 }
