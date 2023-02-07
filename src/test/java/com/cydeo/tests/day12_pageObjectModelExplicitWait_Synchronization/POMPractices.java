a47c283d764cdc0bf9f72c5b5bef984743da876c
Mehmet Usta <ustamehmet83@hotmail.com>
2023-02-07 23:16:01 +0300

    day12 LibraryLoginPages.java

--- src/test/java/com/cydeo/tests/day12_pageObjectModelExplicitWait_Synchronization/POMPractices.java
+++ src/test/java/com/cydeo/tests/day12_pageObjectModelExplicitWait_Synchronization/POMPractices.java
@@ -3,10 +3,21 @@ package com.cydeo.tests.day12_pageObjectModelExplicitWait_Synchronization;
 import com.cydeo.pages.LibraryLoginPages;
 import com.cydeo.utilities.Driver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class POMPractices {
     LibraryLoginPages libraryLoginPages;
+    @BeforeMethod
+    public void setupMethod(){
+        Driver.getDriver().get("https://library1.cydeo.com");
+        libraryLoginPages=new LibraryLoginPages();
+    }
+    @AfterMethod
+    public void close(){
+        Driver.closeDriver();
+    }
 
     @Test
     public void required_field_error_message_test(){
@@ -14,15 +25,12 @@ public class POMPractices {
         //TC #1: Required field error message test
         //1- Open a Chrome browser
         //2- Go to: https://library1.cydeo.com
-        Driver.getDriver().get("https://library1.cydeo.com");
         //3- Do not enter any information
         //4- Click to “Sign in” button
-        libraryLoginPages=new LibraryLoginPages();
         libraryLoginPages.signInButton.click();
         //5- Verify expected error is displayed:
         //Expected: This field is required.
         Assert.assertTrue(libraryLoginPages.fieldRequiredErrorMessage.isDisplayed());
-        Driver.closeDriver();
         //NOTE: FOLLOW POM DESIGN PATTERN
 
     }
@@ -34,15 +42,12 @@ public class POMPractices {
         //TC #2: Invalid email format error message test
         //1- Open a Chrome browser
         //2- Go to: https://library1.cydeo.com
-        Driver.getDriver().get("https://library1.cydeo.com");
         //3- Enter invalid email format
-        libraryLoginPages=new LibraryLoginPages();
         libraryLoginPages.inputUsername.sendKeys("somethingwrong");
         libraryLoginPages.signInButton.click();
         //4- Verify expected error is displayed:
         Assert.assertTrue(libraryLoginPages.enterValidEmailErrorMessage.isDisplayed());
         //Expected: Please enter a valid email address.
-        Driver.closeDriver();
         //
         //NOTE: FOLLOW POM DESIGN PATTERN
     }
@@ -52,15 +57,12 @@ public class POMPractices {
         //TC #3: Library negative login
         //1- Open a Chrome browser
         //2- Go to: https://library1.cydeo.com
-        libraryLoginPages=new LibraryLoginPages();
-        Driver.getDriver().get("https://library1.cydeo.com");
         //3- Enter incorrect username or incorrect password
         libraryLoginPages.inputUsername.sendKeys("wrong@username.com");
         libraryLoginPages.inputPassword.sendKeys("somethingwrong");
         libraryLoginPages.signInButton.click();
         //4- Verify title expected error is displayed:
         Assert.assertTrue(libraryLoginPages.wrongEmailOrPasswordErrorMessage.isDisplayed());
-        Driver.closeDriver();
         //Expected: Sorry, Wrong Email or Password
         //
         //NOTE: FOLLOW POM DESIGN PATTERN
---
+++ src/test/java/com/cydeo/tests/day12_pageObjectModelExplicitWait_Synchronization/WebDriverWaitPractice.java
@@ -0,0 +1,41 @@
+package com.cydeo.tests.day12_pageObjectModelExplicitWait_Synchronization;
+
+import com.cydeo.pages.DynamicLoad7Page;
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class WebDriverWaitPractice {
+    DynamicLoad7Page dynamicTitle;
+    WebDriverWait wait;
+
+    @BeforeMethod
+    public void setUp(){
+        Driver.getDriver().get("https://practice.cydeo.com/dynamic_loading/7");
+        //creating the wait object to be able to create certain condition to wait
+        wait=new WebDriverWait(Driver.getDriver(),10);
+        dynamicTitle=new DynamicLoad7Page();
+    }
+    @Test
+    public void dynamically_Loaded_Page_Elements(){
+        //TC#4 : Dynamically Loaded Page Elements 7
+        //1. Go to https://practice.cydeo.com/dynamic_loading/7
+        //2. Wait until title is “Dynamic title”
+        wait.until(ExpectedConditions.titleIs("Dynamic title"));
+        //3. Assert:
+        //Message “Done” is displayed.
+        Assert.assertTrue(dynamicTitle.doneMessage.isDisplayed());
+        //4. Assert: Image is displayed.
+        Assert.assertTrue(dynamicTitle.spongeBobImage.isDisplayed());
+        //Note: Follow POM
+
+    }
+
+
+
+
+
+}
